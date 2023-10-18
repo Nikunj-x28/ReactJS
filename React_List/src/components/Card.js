46d5e2b26b4b324b2e1e82828f654bee31f269c5
Nikunj <nikunjmi123@gmail.com>
2023-10-18 20:17:07 +0530

    Version 0.5(unfinished) List

---
+++ React_List/src/components/Card.js
@@ -0,0 +1,6 @@
+import './Card.css'
+function Card(props){
+    // const classes = 'card ' + props.className ;
+    return <div className={`card ${props.className}`}>{props.children}</div>;
+}
+export default Card;
