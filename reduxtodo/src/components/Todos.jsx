c08aa02578468439572bb47c034dd4b52e7262cd
Nikunj <nikunjmi123@gmail.com>
2024-06-06 15:16:42 +0530

    added a small app using rtk

---
+++ reduxtodo/src/components/Todos.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { removeTodo } from "../features/todo/todoSlice";
+const Todos = () => {
+  const todos = useSelector((state) => {
+    return state.todos;
+  });
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <div>Todos</div>
+      <ul>
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              {todo.text}
+              <button
+                onClick={() => {
+                  dispatch(removeTodo(todo.id));
+                }}
+              >
+                Remove
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Todos;
